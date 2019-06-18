2957a04f2c8eff102009ed0b30e375929f35a851
cnguyen <cnguyenc@seas.upenn.edu>
2019-06-17 18:37:19 -1000

    Removing unwanted file.

--- Week4/src/GitHubPractice.java
+++
@@ -1,9 +0,0 @@
-
-public class GitHubPractice {
-	public static void main(String[] args) {
-		System.out.println("Hello there, Collaborators.  Please make a change on this println, add, commit, then push."
-				+ " Afterwards I'll do the same and then we should have a merge error, which is expected for this exercise."
-				+ " I'll edit the change and then merge it finally.");
-	}
-
-}
