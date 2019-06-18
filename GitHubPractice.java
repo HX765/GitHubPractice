21892699b478eab4a741ea28ec6e71eec87cb396
cnguyen <cnguyenc@seas.upenn.edu>
2019-06-17 18:59:09 -1000

    Added another string.

--- GitHubPractice.java
+++ GitHubPractice.java
@@ -4,6 +4,7 @@ public class GitHubPractice {
 		System.out.println("Hello there, Collaborators.  Please make a change on this println, add, commit, then push."
 				+ " Afterwards I'll do the same and then we should have a merge error, which is expected for this exercise."
 				+ " I'll edit the change and then merge it finally.");
+		System.out.println("1 + 1 = 11");		
 	}
 
 }
